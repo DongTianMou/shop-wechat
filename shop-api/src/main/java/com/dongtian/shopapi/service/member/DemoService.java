db831ea8ff85cea61328968269ef70bbcd66c7ef
dongtian <707493342@qq.com>
2019-11-13 13:08:17 +0800

    version2.0

--- shop-api/src/main/java/com/dongtian/shopapi/service/member/DemoService.java
+++ shop-api/src/main/java/com/dongtian/shopapi/service/member/DemoService.java
@@ -3,8 +3,15 @@ package com.dongtian.shopapi.service.member;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Map;
+
 @RequestMapping("/demo")
 public interface DemoService {
     @GetMapping("/hello")
     public String hello();
+    @GetMapping("/setKey")
+    public Map<String,Object> setKey();
+    @GetMapping("/getKey")
+    public Map<String,Object> getKey();
+
 }
