6f9f8dd491c9d45e4f8b6ff78e51c0bd58495d41
dongtian <707493342@qq.com>
2019-11-13 13:10:29 +0800

    version1.0

--- shop-member/src/main/java/com/dogtian/shopmember/ShopMemberApplication.java
+++ shop-member/src/main/java/com/dogtian/shopmember/ShopMemberApplication.java
@@ -7,9 +7,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient
 public class ShopMemberApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ShopMemberApplication.class, args);
     }
-
 }
