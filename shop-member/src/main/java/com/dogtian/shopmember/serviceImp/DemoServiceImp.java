b3a67a5e975c05772806e4243ff9cdde0b0e9da6
dongtian <707493342@qq.com>
2019-11-13 13:10:14 +0800

    version1.0

--- shop-member/src/main/java/com/dogtian/shopmember/serviceImp/DemoServiceImp.java
+++ shop-member/src/main/java/com/dogtian/shopmember/serviceImp/DemoServiceImp.java
@@ -3,11 +3,25 @@ package com.dogtian.shopmember.serviceImp;
 import com.dongtian.shopapi.service.member.DemoService;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 public class DemoServiceImp implements DemoService {
 
     @Override
     public String hello() {
         return "hello";
+    }
+
+    @Override
+    public Map<String, Object> setKey() {
+        System.out.println("set key success");
+        return null;
+    }
+
+    @Override
+    public Map<String, Object> getKey() {
+        System.out.println("get key success");
+        return null;
     }
 }
